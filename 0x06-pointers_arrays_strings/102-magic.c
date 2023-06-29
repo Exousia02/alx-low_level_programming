09548c38553fb497125a962a1af088f8a9f7575e
Exousia02 <olorunmola@gmail.com>
2023-06-29 15:40:37 -0700

    task 10

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -16,7 +16,7 @@ p = &n;
 * - you are not allowed to code anything else than this line of code
 */
 Main - print a[2] = 98
-return (0);
+return -0
 /* ...so that this prints 98\n */
 printf("a[2] = %d\n", a[2]);
 return (0);

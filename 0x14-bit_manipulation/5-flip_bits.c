df3c8b9836c7f00944dee3062cbdc675bf9d0c68
Exousia02 <Exousia02>
2023-08-03 07:50:41 -0700

    5-flip_bits.c

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,17 +1,17 @@
 #include "main.h"
 /**
-* flip_bits - counts the number of bits to change
-* to get from one number to another
-* @n: first number
-* @m: second number
+* flip_bits - counts number of bits to convert
+* In order to derive from one number to the other
+* @j: first number
+* @k: second number
 *
-* Return: number of bits to change
+* Return: It returns the number of bits to convert
 */
-unsigned int flip_bits(unsigned long int n, unsigned long int m)
+unsigned int flip_bits(unsigned long int j, unsigned long int k)
 {
 int i, count = 0;
 unsigned long int current;
-unsigned long int exclusive = n ^ m;
+unsigned long int exclusive = j ^ k;
 for (i = 63; i >= 0; i--)
 {
 current = exclusive >> i;
